cac50df463d98dbdbbdb976252e96ff958af02f3
이창보 <101192100+jacobhboy@users.noreply.github.com>
2023-07-05 10:17:05 +0900

    crawling setting

--- src/domain/crawling/crawling.ts
+++ src/domain/crawling/crawling.ts
@@ -1,10 +1,11 @@
 import * as console from "console";
+import {defaults} from "axios";
 
 const axios = require("axios");
 const cheerio = require("cheerio");
 
 
-const getHtml = async (companyName: string) => {
+const companyInfoCrawling = async (companyName: string) => {
     try {
         const injae: String = await axios.get('https://www.google.com/search?q=' + companyName + "인재상");
         const culture: String = await axios.get('https://www.google.com/search?q=' + companyName + "문화").data;
@@ -17,5 +18,7 @@ const getHtml = async (companyName: string) => {
     }
 };
 
-getHtml("카카오").then(()=>{
+companyInfoCrawling("카카오").then(()=>{
 })
+
+export default companyInfoCrawling
